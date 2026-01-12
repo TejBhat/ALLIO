19fa33dbb2137106fc40449c6b099aa3b24ea68c
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-12 22:39:59 +0530

    feat: add auth page

--- ALLIO/app/auth.tsx
+++ ALLIO/app/auth.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { KeyboardAvoidingView, Platform, View} from "react-native";
+import { KeyboardAvoidingView, Platform, View,StyleSheet} from "react-native";
 import {Button, Text, TextInput} from "react-native-paper";
 
 
@@ -11,26 +11,61 @@ export default function AuthScreen(){
         setIsSignUp((prev)=>!prev);
     }
     return (
-    <KeyboardAvoidingView behavior={Platform.OS==="android"?"padding":"height"}>
-        <View>
-            <Text>{isSignUp? "Create Account":"Welcome Back"}</Text>
+    <KeyboardAvoidingView behavior={Platform.OS==="android"?"padding":"height"} style={style.container}>
+
+        <View style={style.content}>
+            <Text style={style.title} variant="headlineMedium">{isSignUp? "Create Account":"Welcome Back"}</Text>
             <TextInput 
             label="Email" 
             autoCapitalize="none" 
             keyboardType="email-address" 
             placeholder="example@gmail.com"
-            mode="outlined"/>
+            mode="outlined"
+            style={style.inputs}/>
 
              <TextInput 
             label="Password" 
             autoCapitalize="none" 
             keyboardType="email-address" 
-            mode="outlined"/>
+            mode="outlined"
+            style={style.inputs}/>
 
-            <Button mode="contained">{isSignUp? "Sign Up":"Sign In"}</Button>
-            <Button mode="text" onPress={handleSwitchMode}>{isSignUp?"Already have an account? Sign In":"Don't have an account? Sign Up"}</Button>
+            <Button mode="contained" style={style.button}>{isSignUp? "Sign Up":"Sign In"}</Button>
+            <Button mode="text" onPress={handleSwitchMode} style={style.switchModeButton}>{isSignUp?"Already have an account? Sign In":"Don't have an account? Sign Up"}</Button>
         </View>
 
     </KeyboardAvoidingView>
     );
 }
+
+const style=StyleSheet.create({
+    container:{
+             flex:1,
+             backgroundColor:"#f5f5f5"
+    },//container is for whole screen!
+
+    content:{
+        flex:1,
+        padding:16,
+        justifyContent:"center"
+    },//content for the whole view of the screen
+
+     title:{
+       textAlign:"center",//brings the text in the center
+       marginBottom:24, //some distance from inputs
+    },//title for text Welcome Back
+
+    inputs:{
+       marginBottom:16,
+    },
+
+    button:{
+       marginTop:8,
+    },
+
+    switchModeButton:{
+        marginTop:16,
+    },
+
+
+});
