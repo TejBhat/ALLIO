d3198f0b3736b9b51ccad6bcd058ac943b8b6c2c
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-14 22:31:12 +0530

    feat: add dashboard page

--- app/(app)/dashboard.tsx
+++ app/(app)/dashboard.tsx
@@ -1,9 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
 
 export default function DashboardScreen(){
     return(
-     <View>
-        <Text>Dashboard</Text>
+     <View style={style.container}>
+        <View style={style.grid}>
+            <Card>
+                <Text>Idea Notes</Text>
+            </Card>
+
+            <Card>
+                <Text>Calender Check</Text>
+            </Card>
+
+            <Card>
+                <Text>Water Intake</Text>
+            </Card>
+
+            <Card>
+                <Text>Today/Overview</Text>
+            </Card>
+        </View>
      </View>
     );
 }
+const style=StyleSheet.create({
+    container:{
+       flex:1,
+       padding:16,
+       backgroundColor:"#fffaf3",
+       justifyContent:"center",
+    },
+    grid:{
+         flexDirection:"row",
+         flexWrap:"wrap",
+         justifyContent:"space-between",
+    },
+});
