332d669635d06845caf8302de007904bddf3de04
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-15 11:16:03 +0530

    feat: add dashboard icons

--- app/(app)/dashboard.tsx
+++ app/(app)/dashboard.tsx
@@ -1,5 +1,9 @@
 import { View, Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function DashboardScreen(){
     return(
@@ -7,18 +11,22 @@ export default function DashboardScreen(){
         <Card style={style.bigCard}>
             <View style={style.grid}>
             <Card style={style.card}>
+                <SimpleLineIcons name="notebook" size={24} color="black" />
                 <Text style={style.cardText}>Idea Notes</Text>
             </Card>
 
             <Card style={style.card}>
+                <FontAwesome name="calendar-check-o" size={24} color="black" />
                 <Text style={style.cardText}>Calender Check</Text>
             </Card>
 
             <Card style={style.card}>
+                <MaterialCommunityIcons name="water-check" size={24} color="black" />
                 <Text style={style.cardText}>Water Intake</Text>
             </Card>
 
             <Card style={style.card}>
+                <Ionicons name="today-outline" size={24} color="black" />
                 <Text style={style.cardText}>Today/Overview</Text>
             </Card>
         </View>
@@ -31,13 +39,16 @@ const style=StyleSheet.create({
     container:{
        flex:1,
        padding:18,
-       backgroundColor:"#5e4522",
+       backgroundColor:"#ffffff",
        justifyContent:"center",
     },
+
     bigCard:{
-       padding:16,
+       margin:1,
+       marginBottom:40,
+       padding:20,
        borderRadius:16,
-       backgroundColor:"#b08848",
+       backgroundColor:"offwhitec",
        justifyContent:"center",
     },
 
@@ -47,19 +58,19 @@ const style=StyleSheet.create({
          justifyContent:"space-between",
     },
     card:{
-        width:"47%",
+        width:"45%",
         height:140,
-        marginBottom:18,
-        borderRadius:16,
+        marginBottom:40,
+        borderRadius:12,
         justifyContent:"center",
         alignItems:"center",
-        backgroundColor:"#7a4a00",
-        elevation:4,
+        backgroundColor:"#6b4726",
+        gap:6,
 
     },
 
     cardText:{
-        color:"#ffd84d",
+        color:"yellow",
         fontSize:16,
         fontWeight:"800",
         textAlign:"center",
