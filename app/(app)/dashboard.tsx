8bf28224f88578a643233c7ad54b78cdd3f195ad
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-15 14:18:48 +0530

    feat: add menu slide

--- app/(app)/dashboard.tsx
+++ app/(app)/dashboard.tsx
@@ -1,17 +1,39 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable, Animated} from "react-native";
 import { Card } from "react-native-paper";
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Octicons from '@expo/vector-icons/Octicons';
+import { useRef,useState } from "react"; 
 
 export default function DashboardScreen(){
+      const [menuOpen, setMenuOpen]=useState(false);
+      const slide=useRef(new Animated.Value(-260)).current;
+      
+      const toggleMenu=()=>{
+        Animated.timing(slide,{
+            toValue:menuOpen?-260:0,
+            duration:250,
+            useNativeDriver:true,
+        }).start();
+        setMenuOpen(!menuOpen);
+      };
+
+
     return(
      <View style={style.container}>
         <View style={style.header}>
-        <Octicons name="three-bars" size={24} color="black" />
+            <Pressable onPress={toggleMenu}>
+                <Octicons name="three-bars" size={24} color="black" />
+            </Pressable>
         </View>
+        <Animated.View style={[style.sideMenu,{transform:[{translateX:slide}]},]}>
+            <Text style={style.menuItems}>Account</Text>
+             <Text style={style.menuItems}>About ALLIO</Text>
+            <Text style={style.menuItems}>Appearance</Text>
+            <Text style={[style.menuItems,{color:"red"}]}>Logout</Text>
+        </Animated.View>
         <Card style={style.bigCard}>
             <View style={style.grid}>
             <Card style={style.card}>
@@ -84,5 +106,24 @@ const style=StyleSheet.create({
         fontWeight:"800",
         textAlign:"center",
 
+    },
+
+    sideMenu:{
+        position:"absolute",
+        top:0,
+        left:0,
+        width:260,
+        height:"100%",
+        backgroundColor:"#f7f3ee",
+        paddingTop:80,
+        paddingHorizontal:20,
+        zIndex:10,
+        elevation:10,
+    },
+    menuItems:{
+       fontSize:18,
+       fontWeight:"600",
+       marginBottom:24,
+       color:"#333",
     },
 });
