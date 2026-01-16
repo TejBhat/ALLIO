fb5e6ee0b65dea7c42b67b5e11578637f3893a69
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-16 19:53:24 +0530

    feat: add navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,13 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{headerShown: false}}>
-      <Stack.Screen name="index" />
-    <Stack.Screen name="auth" />
+   <Stack.Screen name="/index"/>
+   <Stack.Screen name="/auth"/>
+   <Stack.Screen name="/dashboard"/>
+   <Stack.Screen name="/notes" options={{title:"Notes"}}/>
+   <Stack.Screen name="/calendarcheck" options={{title:"Calendarcheck"}}/>
+   <Stack.Screen name="/waterintake" options={{title:"WaterCheckList"}}/>
+   <Stack.Screen name="/overview" options={{title:"Overview"}}/>
     </Stack>
   );
 }
