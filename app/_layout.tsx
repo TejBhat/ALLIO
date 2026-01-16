5e748ddfb3245fdda221f8024f8ddfbf5f224526
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-16 22:14:46 +0530

    feat: add navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,20 @@
 import { Stack } from "expo-router";
+import { ThemeProvider } from "./context/ThemeContext";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{headerShown: false}}>
-   <Stack.Screen name="/index"/>
-   <Stack.Screen name="/auth"/>
-   <Stack.Screen name="/dashboard"/>
-   <Stack.Screen name="/notes" options={{title:"Notes"}}/>
-   <Stack.Screen name="/calendarcheck" options={{title:"Calendarcheck"}}/>
-   <Stack.Screen name="/waterintake" options={{title:"WaterCheckList"}}/>
-   <Stack.Screen name="/overview" options={{title:"Overview"}}/>
-    </Stack>
+    <ThemeProvider>
+      <Stack screenOptions={{headerShown: false}}>
+        <Stack.Screen name="/index"/>
+        <Stack.Screen name="/auth"/>
+        <Stack.Screen name="/dashboard"/>
+        <Stack.Screen name="/notes" options={{title:"Notes"}}/>
+        <Stack.Screen name="/calendarcheck" options={{title:"Calendarcheck"}}/>
+        <Stack.Screen name="/waterintake" options={{title:"WaterCheckList"}}/>
+        <Stack.Screen name="/overview" options={{title:"Overview"}}/>
+        <Stack.Screen name="/about" options={{title:"About ALLIO"}}/>
+        <Stack.Screen name="/account" options={{title:"Account"}}/>
+      </Stack>
+    </ThemeProvider>
   );
 }
